751a21be35533ea361c2513c51f6e39ee40fec4c
Croped <lhaemmerle@gmx.at>
2019-06-28 18:14:08 +0200

    Character + Player Script
    Base Functions

---
+++ SkeletonSlayerUnity/Assets/Scripts/Character.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public int HP_Base;
+    [HideInInspector] public int HP_Current;
+    public int DMG_Base;
+    [HideInInspector] public int DMG_Current;
+    public int MoveSpeed_Base;
+    [HideInInspector] public int MoveSpeed_Current;
+    public int JumpForce_Base;
+    [HideInInspector] public int JumpForce_Current;
+
+    public LayerMask GroundLayer;
+
+    private Rigidbody2D RB;
+
+    private void Awake()
+    {
+        RB = GetComponent<Rigidbody2D>();
+        HP_Current = HP_Base;
+        DMG_Current = DMG_Base;
+        MoveSpeed_Current = MoveSpeed_Base;
+        JumpForce_Current = JumpForce_Base;
+    }
+
+    public void Move(Vector2 direction)
+    {
+        RB.velocity = new Vector2(direction.x * MoveSpeed_Current, RB.velocity.y);
+    }
+
+    public bool GroundCheck()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, GetComponent<Renderer>().bounds.size.y * 0.52f, GroundLayer);
+        if (hit)
+        {
+            Debug.Log("GroundCheck - Success");
+            return true;
+        }
+        else
+        {
+            Debug.Log("GroundCheck - Failed");
+            if (hit)
+                Debug.Log("Raycasthit Name:" + hit.collider.name);
+            return false;
+        }
+    }
+
+    public void Jump()
+    {
+        if (GroundCheck())
+        {
+            RB.AddForce(Vector2.up * JumpForce_Current);
+        }
+    }
+}
---
+++ SkeletonSlayerUnity/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            Move(Input.GetAxis("Horizontal") > 0 ? Vector2.right : Vector2.left);
+        }
+        else
+        {
+            Move(Vector2.zero);
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+}
