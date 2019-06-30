9e59d41aced90192deef51deb9566151cc4821bd
Simon Blaha <s.blaha@live.at>
2019-06-30 16:27:34 +0200

    add: jump to new mvmt script

--- SkeletonSlayerUnity/Assets/Scripts/BasicMovementMage.cs
+++ SkeletonSlayerUnity/Assets/Scripts/BasicMovementMage.cs
@@ -4,11 +4,8 @@ using UnityEngine;
 
 public class BasicMovementMage : MonoBehaviour
 
-    
-
-
+  
 {
-
     public float moveSpeed = 5f;
 
     // Start is called before the first frame update
@@ -18,14 +15,12 @@ public class BasicMovementMage : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+        Jump();
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;
 
-        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
-        transform.position += horizontal * Time.deltaTime * moveSpeed;
-
-        void Jump()
-        {
+        void Jump() {
             if(Input.GetButtonDown("Jump"))
             {
                 gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
