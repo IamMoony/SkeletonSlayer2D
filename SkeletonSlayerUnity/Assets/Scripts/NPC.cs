d98b757e93249a7217740213757c098d4029c054
Croped <lhaemmerle@gmx.at>
2019-06-30 15:39:26 +0200

    Patrol Coroutine for NPC's

--- SkeletonSlayerUnity/Assets/Scripts/NPC.cs
+++ SkeletonSlayerUnity/Assets/Scripts/NPC.cs
@@ -4,5 +4,46 @@ using UnityEngine;
 
 public class NPC : Character
 {
-    public enum attitude {Friendly, Neutral, Enemy};
+    public float viewDistance;
+
+    public GameObject target;
+
+    public enum attitude { Friendly, Neutral, Enemy };
+    public attitude Attitude;
+
+    public enum state {Normal, Alert}
+    public state State;
+
+    public enum action {Guarding, Patrolling, Attacking, Searching, Fleeing};
+    public action Action;
+
+    public IEnumerator Patrol()
+    {
+        Move(transform.right);
+        RaycastHit2D viewCheck = Physics2D.Raycast(transform.position + transform.right * 0.5f, transform.right, viewDistance);
+        RaycastHit2D groundCheck = Physics2D.Raycast(transform.position + transform.right * 0.5f, Vector2.down, 10f);
+        while (target == null)
+        {
+            if (groundCheck)
+            {
+                if (viewCheck)
+                {
+                    Move(Vector2.zero);
+                    if (viewCheck.collider.tag == "Player")
+                    {
+                        target = viewCheck.collider.gameObject;
+                    }
+                    else
+                    {
+                        Turn();
+                    }
+                }
+            }
+            else
+            {
+                Turn();
+            }
+            yield return 10;
+        }
+    }
 }
